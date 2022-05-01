487d893a7d073627a3ffca67323a8fb1fc06731a
Choiwoosung34 <myhouse34@naver.com>
2022-05-01 22:38:48 +0900

    Spring Data Jpa 로 마이그레이션

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -1,7 +1,7 @@
 package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.Member;
-import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.MemberRepositoryOld;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,7 +17,7 @@ class MemberServiceTest {
     protected MemberService memberService;
 
     @Autowired
-    protected MemberRepository memberRepository;
+    protected MemberRepositoryOld memberRepository;
 
     @Test
     void 회원가입() throws Exception {
